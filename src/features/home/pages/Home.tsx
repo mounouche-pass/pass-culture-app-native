43daa551959c5d97019159adb04ce0d2e2bcd76f
Ali RAIKI <ali.raiki@polyconseil.fr>
2022-10-07 14:27:57 +0200

    test without progressive loading

--- src/features/home/pages/Home.tsx
+++ src/features/home/pages/Home.tsx
@@ -1,12 +1,6 @@
 import { useRoute } from '@react-navigation/native'
-import React, { useCallback, useEffect, useState, FunctionComponent, memo } from 'react'
-import {
-  FlatList,
-  ScrollView,
-  NativeScrollEvent,
-  NativeSyntheticEvent,
-  Platform,
-} from 'react-native'
+import React, { useCallback, FunctionComponent, memo } from 'react'
+import { FlatList, ScrollView, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
 import styled from 'styled-components/native'
 
 import { useHomepageModules } from 'features/home/api'
@@ -32,9 +26,7 @@ import useFunctionOnce from 'libs/hooks/useFunctionOnce'
 import { useNetInfoContext } from 'libs/network/NetInfoWrapper'
 import { OfflinePage } from 'libs/network/OfflinePage'
 import { BatchEvent, BatchUser } from 'libs/react-native-batch'
-import { theme } from 'theme'
-import { Spinner } from 'ui/components/Spinner'
-import { getSpacing, Spacer } from 'ui/theme'
+import { Spacer } from 'ui/theme'
 
 const keyExtractor = (item: ProcessedModule, index: number) =>
   'moduleId' in item ? item.moduleId : `recommendation${index}`
@@ -116,19 +108,6 @@ const renderModule = (
   homeEntryId: string | undefined
 ) => <Module item={item} index={index} homeEntryId={homeEntryId} />
 
-const FooterComponent = ({ isLoading }: { isLoading: boolean }) => {
-  return (
-    <React.Fragment>
-      {!!isLoading && (
-        <FooterContainer>
-          <Spinner />
-        </FooterContainer>
-      )}
-      <Spacer.TabBar />
-    </React.Fragment>
-  )
-}
-
 export const OnlineHome: FunctionComponent = () => {
   const { params } = useRoute<UseRouteType<'Home'>>()
   const { notificationsTrigger } = useRemoteConfigContext()
@@ -139,41 +118,18 @@ export const OnlineHome: FunctionComponent = () => {
     BatchUser.trackEvent(BatchEvent.hasSeenAllTheHomepage)
   )
   const showSkeleton = useShowSkeleton()
-  const initialNumToRender = 10
-  const maxToRenderPerBatch = 5
-  const [maxIndex, setMaxIndex] = useState(initialNumToRender)
-  const [isLoading, setIsLoading] = useState(false)
-  const modulesToDisplay = Platform.OS === 'web' ? modules : modules.slice(0, maxIndex)
+  const initialNumToRender = 5
 
   const onScroll = useCallback(
     ({ nativeEvent }: NativeSyntheticEvent<NativeScrollEvent>) => {
-      if (isCloseToBottom({ ...nativeEvent, padding: theme.minScreenHeight * 2 })) {
-        if (Platform.OS !== 'web' && maxIndex < modules.length) {
-          setIsLoading(true)
-          setMaxIndex(maxIndex + maxToRenderPerBatch)
-        }
-      }
-      if (isCloseToBottom(nativeEvent) && modulesToDisplay.length === modules.length) {
+      if (isCloseToBottom(nativeEvent)) {
         trackEventHasSeenAllModules()
         logHasSeenAllModules()
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [modules.length, modulesToDisplay.length]
+    [modules.length]
   )
-
-  useEffect(() => {
-    // We use this to load more modules, in case the content size doesn't change after the load triggered by onEndReached (i.e. no new modules were shown).
-    const loadMore = setInterval(() => {
-      if (maxIndex < modules.length && isLoading) {
-        setMaxIndex(maxIndex + maxToRenderPerBatch)
-      } else {
-        setIsLoading(false)
-      }
-    }, 3000)
-
-    return () => clearInterval(loadMore)
-  }, [modules.length, isLoading, maxIndex])
 
   if (notificationsTrigger === NotificationsTrigger.HOME) {
     showPushNotificationsModalForFirstTime()
@@ -198,16 +154,15 @@ export const OnlineHome: FunctionComponent = () => {
       <HomeBodyLoadingContainer hide={showSkeleton}>
         <FlatList
           testID="homeBodyScrollView"
-          scrollEventThrottle={1000}
+          scrollEventThrottle={400}
           onScroll={onScroll}
-          data={modulesToDisplay}
+          data={modules}
           renderItem={({ item, index }) => renderModule({ item, index }, homeEntryId)}
           keyExtractor={keyExtractor}
-          ListFooterComponent={<FooterComponent isLoading={isLoading} />}
+          ListFooterComponent={<Spacer.TabBar />}
           ListHeaderComponent={ListHeaderComponent}
           initialNumToRender={initialNumToRender}
           removeClippedSubviews={false}
-          onContentSizeChange={() => setTimeout(() => setIsLoading(false), 1000)}
           bounces
         />
       </HomeBodyLoadingContainer>
@@ -233,11 +188,6 @@ const Container = styled.View({
   flexBasis: 1,
   flexGrow: 1,
   flexShrink: 0,
-})
-
-const FooterContainer = styled.View({
-  paddingTop: getSpacing(2),
-  paddingBottom: getSpacing(10),
 })
 
 const ListHeaderContainer = styled.View({
