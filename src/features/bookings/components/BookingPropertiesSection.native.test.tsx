4f89e98480b8f615729acc1f8724a7f1ef6edeee
AnoukHello <anoukhello22@gmail.com>
2022-11-28 17:38:38 +0100

    (BSR) bookings: replace superflush and waitforexpect with waitFor

--- src/features/bookings/components/BookingPropertiesSection.native.test.tsx
+++ src/features/bookings/components/BookingPropertiesSection.native.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import waitForExpect from 'wait-for-expect'
 
 import { BookingPropertiesSection } from 'features/bookings/components/BookingPropertiesSection'
 import { bookingsSnap } from 'features/bookings/fixtures/bookingsSnap'
 import { Booking } from 'features/bookings/types'
 import { reactQueryProviderHOC } from 'tests/reactQueryProviderHOC'
-import { superFlushWithAct, render } from 'tests/utils'
+import { render, waitFor } from 'tests/utils'
 
 jest.mock('features/profile/api', () => ({
   useUserProfileInfo: jest.fn(() => ({ data: { firstName: 'Christophe', lastName: 'Dupont' } })),
@@ -17,26 +16,26 @@ describe('<BookingPropertiesSection />', () => {
   const booking = bookingsSnap.ongoing_bookings[0]
 
   it('should display user firstname and lastname', async () => {
-    const { getByText } = await renderBookingProperties(booking)
+    const { getByText } = renderBookingProperties(booking)
 
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(getByText('Christophe\u00a0Dupont')).toBeTruthy()
     })
   })
 
   it('should display duo icon when offer is duo', async () => {
     booking.quantity = 2
-    const { getByTestId } = await renderBookingProperties(booking)
+    const { getByTestId } = renderBookingProperties(booking)
 
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(getByTestId('duo-icon')).toBeTruthy()
     })
   })
 
   it('should display date label', async () => {
-    const { getByText } = await renderBookingProperties(booking)
+    const { getByText } = renderBookingProperties(booking)
 
-    await waitForExpect(() => {
+    await waitFor(() => {
       expect(getByText('Le 15 mars 2021 à 20h00')).toBeTruthy()
     })
   })
@@ -44,16 +43,15 @@ describe('<BookingPropertiesSection />', () => {
   it('should display location label if offer is not permanent and not a digital event', async () => {
     booking.stock.offer.isDigital = false
     booking.stock.offer.isPermanent = false
-    const { getByText } = await renderBookingProperties(booking)
-    await waitForExpect(() => {
+    const { getByText } = renderBookingProperties(booking)
+    await waitFor(() => {
       expect(getByText('Maison de la Brique, Drancy')).toBeTruthy()
     })
   })
 })
 
-async function renderBookingProperties(booking: Booking) {
+function renderBookingProperties(booking: Booking) {
   // eslint-disable-next-line local-rules/no-react-query-provider-hoc
   const wrapper = render(reactQueryProviderHOC(<BookingPropertiesSection booking={booking} />))
-  await superFlushWithAct()
   return wrapper
 }
